cc5571f49fd61035a6b262e2b541c253ba19fefd
Sumeet1308 <63195012+Sumeet1308@users.noreply.github.com>
2020-04-05 23:24:00 +0530

    Small change1

--- Classes.py
+++ Classes.py
@@ -7,7 +7,7 @@ class Computer:
     def config(self):
         print ("The configuration is ",self.ram," RAM and ",self.CPU," CPU")
 
-c1=Computer("9GB")
+c1=Computer("8GB")
 c2=Computer("8GB","AMD")
 c1.CPU="I3"
 c1.config()
